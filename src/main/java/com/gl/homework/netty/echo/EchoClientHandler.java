8af13332a3de516caffd64a552030b3f93f2c105
gavin <gavin2lee@163.com>
2016-12-18 09:48:00 +0800

    init

---
+++ src/main/java/com/gl/homework/netty/echo/EchoClientHandler.java
@@ -0,0 +1,57 @@
+package com.gl.homework.netty.echo;
+
+import java.io.UnsupportedEncodingException;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+
+	private final ByteBuf firstMessage;
+
+	/**
+	 * Creates a client-side handler.
+	 */
+	public EchoClientHandler() {
+		firstMessage = Unpooled.buffer(EchoClient.SIZE);
+		for (int i = 0; i < firstMessage.capacity(); i++) {
+			firstMessage.writeByte((byte) i);
+		}
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+		try {
+			ctx.writeAndFlush(Unpooled.buffer(EchoClient.SIZE).writeBytes("AAAAAAAAAAA".getBytes("UTF-8")));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		System.out.println("Client read:" + msg);
+		ctx.writeAndFlush(msg);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		System.out.println("client");
+		ctx.flush();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // Close
+																				// the
+																				// connection
+																				// when
+																				// an
+																				// exception
+																				// is
+																				// raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
---
+++ src/main/java/com/gl/homework/netty/echo/EchoServerHandler.java
@@ -0,0 +1,27 @@
+package com.gl.homework.netty.echo;
+
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 处理服务端 channel.
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { 
+    	System.out.println(msg.getClass().getName());
+    	System.out.println(ctx.channel().remoteAddress()+"->Server :"+ msg.toString());
+        ctx.writeAndFlush(msg); // (1)
+        ctx.channel().writeAndFlush("Msg from Server");
+//        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { 
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
